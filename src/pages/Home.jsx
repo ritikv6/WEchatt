a5d8f17f4a43121673074bfabca6d7104a0893ce
ritikv6 <ritikverma5767@gmail.com>
2026-06-06 18:50:14 +0530

    backend ready

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import {Link} from 'react-router-dom';
 
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import Feature from "../components/Feature";
+import WhyChoose from "../components/Whychoose";
 import Cta from "../components/Cta";
 import Footer from "../components/Footer";
 
