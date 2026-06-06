363fd0db461e70fb295679b0ee32e92031eee693
ritikv6 <ritikverma5767@gmail.com>
2026-06-06 12:52:15 +0530

    fixing deployment-0

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from "./pages/Home";
 import Chat from "./pages/Chat";
 import Room from "./pages/Room";
-import { HashRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <HashRouter>
-    <Router>
+  
+   
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/room/:roomId" element={<Room />} />
       </Routes>
-    </Router>
-    </HashRouter>
+   
+   
   );
 }
 
