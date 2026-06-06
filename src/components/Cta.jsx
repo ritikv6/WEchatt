a4cc7293cf71ddfc9b69c03d181ed249b829c13c
ritikv6 <ritikverma5767@gmail.com>
2026-06-06 20:32:17 +0530

    Fixing code

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './all.css';
 import bgImage from '../assets/cta.png';
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 function Navbar() {
